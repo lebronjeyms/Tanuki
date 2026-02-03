212c76cc84f5cbe007f7a677e336dbd860a7261b
2lwg1cdpiW <aymseygma@gmail.com>
2026-02-03 22:56:14 +0800

    Update KotatsuApp.kt

--- app/src/nightly/kotlin/org/koitharu/kotatsu/KotatsuApp.kt
+++ app/src/nightly/kotlin/org/koitharu/kotatsu/KotatsuApp.kt
@@ -1,34 +1,5 @@
-package org.koitharu.kotatsu
+package org.tanukis.tanuki
 
-import android.content.Context
-import android.content.SharedPreferences
-import androidx.preference.PreferenceManager
-import leakcanary.LeakCanary
-import org.koitharu.kotatsu.core.BaseApp
+import org.tanukis.tanuki.core.BaseApp
 
-class KotatsuApp : BaseApp(), SharedPreferences.OnSharedPreferenceChangeListener {
-
-	override fun attachBaseContext(base: Context) {
-		super.attachBaseContext(base)
-		val prefs = PreferenceManager.getDefaultSharedPreferences(this)
-		configureLeakCanary(isEnabled = prefs.getBoolean(KEY_LEAK_CANARY, false))
-		prefs.registerOnSharedPreferenceChangeListener(this)
-	}
-
-	override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String?) {
-		if (key == KEY_LEAK_CANARY) {
-			configureLeakCanary(sharedPreferences.getBoolean(KEY_LEAK_CANARY, false))
-		}
-	}
-
-	private fun configureLeakCanary(isEnabled: Boolean) {
-		LeakCanary.config = LeakCanary.config.copy(
-			dumpHeap = isEnabled,
-		)
-	}
-
-	private companion object {
-
-		const val KEY_LEAK_CANARY = "debug.leak_canary"
-	}
-}
+class DokiApp : BaseApp()
